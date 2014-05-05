04ce3d5cf30ff5f55f8a040b86c350f9b6ec4162
davepoo <davepoo@hotmail.com>
2014-05-05 19:39:05 +0100

    - fixing compile errors

--- Source/GPGAndroidClient.hpp
+++ Source/GPGAndroidClient.hpp
@@ -3,12 +3,12 @@
 #if defined(_VISION_ANDROID)
 
 #include "VisionBaseIncludes.h"
-#include "IPACloudConnectionClient.hpp"
+#include "PACloudConnectionClient.hpp"
 
 /// \brief
 /// Defines an concrete implementation for for a Project Anarchy Cloud Connection Client.
 /// Provides the Google Play Games Android Client
-class GPGAndroidClient : public IPACloudConnectionClient
+class GPGAndroidClient : public PACloudConnectionClient
 {
   /// \brief
   /// Returns whether or not user is authenticated
--- Source/GPGIOSClient.hpp
+++ Source/GPGIOSClient.hpp
@@ -3,12 +3,12 @@
 #if defined(_VISION_IOS)
 
 #include "VisionBaseIncludes.hpp"
-#include "IPACloudConnectionClient.hpp"
+#include "PACloudConnectionClient.hpp"
 
 /// \brief
 /// Defines an concrete implementation for for a Project Anarchy Cloud Connection Client.
 /// Provides the Google Play Games IOS Client
-class GPGIOSClient : public IPACloudConnectionClient
+class GPGIOSClient : public PACloudConnectionClient
 {
   /// \brief
   /// Returns whether or not user is authenticated

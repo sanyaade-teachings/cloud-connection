76416f4c61185221fec054ebe7b1f1b801b0dfb6
davepoo <davepoo@hotmail.com>
2014-05-06 11:22:46 +0100

    * Adding method declarations to the Google Play Games Clients for Android and iOS

--- Source/GPGAndroidClient.hpp
+++ Source/GPGAndroidClient.hpp
@@ -10,6 +10,7 @@
 /// Provides the Google Play Games Android Client
 class GPGAndroidClient : public PACloudConnectionClient
 {
+public:
   /// \brief
   /// Returns whether or not user is authenticated
   virtual bool IsAuthenticated() HKV_OVERRIDE;
@@ -21,6 +22,33 @@ class GPGAndroidClient : public PACloudConnectionClient
   /// \brief
   /// Returns the authenticated user's display name
   virtual VString GetUserDisplayName() HKV_OVERRIDE;
+
+  /// \brief
+  /// Brings up a platform-specific user authorization flow. 
+  virtual void BeginUserInitiatedSignIn() HKV_OVERRIDE;    
+
+  /// \brief Is there a sign-in process currently in progress
+  /// \return true if an sign-in has started but not yet finished, false otherwise
+  virtual bool IsAuthInProgress() HKV_OVERRIDE;
+
+  /// \brief
+  /// Unlock an achievement for the currently signed in player. 
+  virtual void UnlockAchievement(const VString &achievementId) HKV_OVERRIDE;
+
+  /// \brief
+  /// Presents a UI to the user that displays information about all achievements. 
+  virtual void ShowAchievements() HKV_OVERRIDE;
+    
+  /// \brief
+  /// Submit a score to the leaderboard for the currently signed-in player. 
+  virtual void SubmitHighScore(const VString &leaderboardId, ULONG64 score) HKV_OVERRIDE;
+
+  /// \brief
+  /// Presents a UI to the user that displays information about a specific leaderboard. 
+  virtual void ShowLeaderboard(const VString &leaderboardId) HKV_OVERRIDE;
+
+protected:
+private:
 };
 
 #endif

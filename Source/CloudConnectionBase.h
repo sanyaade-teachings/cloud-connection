4448080d24ab3189bae436f3e5c38f3e6092bfff
davepoo <davepoo@hotmail.com>
2014-05-14 12:11:37 +0100

    * adding a single include for the developer to use the plugin inside their own application

---
+++ Source/CloudConnectionBase.h
@@ -0,0 +1,29 @@
+#ifndef _CLOUDCONNECTIONBASE_H_
+#define _CLOUDCONNECTIONBASE_H_
+
+//This file gives all the includes you need to implement the Cloud Connection Plugin in a game
+
+//Vision classes required to use the client from C++
+#include "CloudConnection.hpp"
+#include "CloudConnectionClient.hpp"
+
+#if defined(_VISION_ANDROID)
+
+//Android required includes for use of cloud connection plugin
+
+//required to initialise the GPG state manager
+#include "GPGAndroidStateManager.hpp"
+#include "gpg/android_initialization.h"
+#include "gpg/debug.h"
+
+#elif defined(_VISION_IOS)
+
+//iOS required includes for use of cloud connection plugin
+
+#else
+
+//No platform defined for cloud connection plugin
+
+#endif
+
+#endif

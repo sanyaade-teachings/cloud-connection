d2ffa7a2fb2e9e43376dad61fd16dad1ea8886b5
davepoo <davepoo@hotmail.com>
2014-06-06 16:53:16 +0100

    * adding some crude custom_rules to copy the libraries and assets into the correct folders for the ANT build (see def copyAssets(options, fatOptions=None): in the deployAndroid python script for how the assets are copied) * updating the build.bat file to accept command from Visual Studio and pass them on to ANT * adding a post build command to run the ant build process once the native stuff is compiled * changing the default google app_id in ids.xml to REPLACE_ME

--- Source/platform/Android/build.bat
+++ Source/platform/Android/build.bat
@@ -4,7 +4,11 @@ REM Build in release mode
 REM call ant release
 
 REM Build in debug mode
+ECHO %1
+ECHO %2
+ECHO %3
+ECHO %4
+ECHO %5
 SET ANDROID_HOME=%ADTROOT%\sdk
-ant debug
-
-PAUSE
+cd .\Platform\Android
+ant debug -D%1=%2 -D%3=%4

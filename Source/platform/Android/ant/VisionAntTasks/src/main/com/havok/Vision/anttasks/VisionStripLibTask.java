35ef61cd4340fb1569aa72fe2839685288b4b021
davepoo <davepoo@hotmail.com>
2014-06-07 13:23:07 +0100

    * adding Vision Ant Tasks project (compiled in Java 1.6) * adding methods to get Vision NDKROOT and ADTROOT * adding method to get the NDK tool chain root * adding method to strip unneeded symbols from a .so library * adding AssetTable to emulate the assetTable.py script * adding VisionAssetCopyTask to emulate the copyAssets function in deployAndroid.py * adding VisionStripLibTask to remove symbols from x86 or arm compiled native binaries * adding unit tests

---
+++ Source/platform/Android/ant/VisionAntTasks/src/main/com/havok/Vision/anttasks/VisionStripLibTask.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package com.havok.Vision.anttasks;
+
+import java.io.File;
+
+import org.apache.tools.ant.BuildException;
+
+/**
+ * ANT Task to strip all symbols from the native library to reduce size and obfuscate file
+ * @author Project Anarchy
+ */
+public class VisionStripLibTask extends VisionBaseTask
+{
+  /* This is the fully name of the processor architecture for the compiled native binary we are going to process */
+  private Architecture m_architecture = null;
+  /** this is the libraray that we are going to process */
+  private File m_library = null;
+  
+  /** ANT Attribute - Required 
+   * This is the fully name of the processor architecture for the compiled native binary we are going to process */
+  public void setArch( String architecture )
+  {
+    if ( architecture.contains("x86") )
+    {
+      m_architecture = Architecture.ARCH_X86;
+    }
+    else
+    {
+      m_architecture = Architecture.ARCH_ARM;
+    }
+  }
+  
+  /** ANT Attribute - Required 
+   * This is the fully qualified path to the the compiled native binary we are going to strib of symbols */
+  public void setLib( File lib )
+  {
+    m_library = lib;
+  }
+  
+  /**
+   * Check that the ANT Attributes are valid
+   * @throws BuildException if any attributes are invalid
+   */
+  void checkANTAttributes() throws BuildException
+  {
+    if ( m_architecture == null )
+    {
+      throw new BuildException("The 'Arch' attribute has not been set, use 'x86' or 'arm'");
+    }
+    
+    if ( m_library == null )
+    {
+      throw new BuildException("The 'Lib' attribute has not been set");
+    }
+    else if ( m_library.exists() == false )
+    {
+      throw new BuildException("The Lib +"+ m_library.getPath() +"' does not exist");
+    }
+    else if ( m_library.isDirectory() )
+    {
+      throw new BuildException("The Lib +"+ m_library.getPath() +"' appears to be a folder, not a file!");
+    }
+  }
+  
+  /**
+   * ANT Task Entry point
+   */
+  @Override
+  public void execute()
+  {
+    checkANTAttributes();    
+    try
+    {
+      int retval = stripLib(m_architecture, m_library.getParent(), m_library.getName() );    
+      if ( retval != 0 )
+      {
+        throw new BuildException("There was an error when stripping the native lib: " + retval);
+      }
+    } 
+    catch (Exception e)
+    {
+      throw new BuildException("There was an error when stripping the native lib", e);
+    }
+  } 
+}

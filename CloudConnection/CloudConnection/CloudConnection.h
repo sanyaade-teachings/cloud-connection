ab5060f5a04b6a1ff08b93c21b967992ca2a11fa
davepoo <davepoo@hotmail.com>
2014-06-10 15:46:24 +0100

    * removing deleted files from iOS project

---
+++ CloudConnection/CloudConnection/CloudConnection.h
@@ -0,0 +1,13 @@
+//
+//  CloudConnection.h
+//  CloudConnection
+//
+//  Created by Dave Poo on 25/05/2014.
+//  Copyright (c) 2014 Havok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CloudConnection : NSObject
+
+@end
---
+++ CloudConnection/CloudConnection/CloudConnection.m
@@ -0,0 +1,13 @@
+//
+//  CloudConnection.m
+//  CloudConnection
+//
+//  Created by Dave Poo on 25/05/2014.
+//  Copyright (c) 2014 Havok. All rights reserved.
+//
+
+#import "CloudConnection.h"
+
+@implementation CloudConnection
+
+@end

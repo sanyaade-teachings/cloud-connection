3d260a447a60d1aff24789e9d583a47111db4440
davepoo <davepoo@hotmail.com>
2014-06-11 13:55:27 +0100

    * fix for win32 compiler warning: removing the cloud connection call back classes and using VisCallback_cl directly instead

--- Source/CloudConnectionCallbacks.cpp
+++ Source/CloudConnectionCallbacks.cpp
@@ -1,18 +1,6 @@
 #include "CloudConnectionPluginPCH.hpp"
 #include "CloudConnectionCallbacks.hpp"
 
-CCOnAuthActionStartedCallback::CCOnAuthActionStartedCallback() {}
-CCOnAuthActionStartedCallback::~CCOnAuthActionStartedCallback() {}
-
-CCOnAuthActionFinishedCallback::CCOnAuthActionFinishedCallback() {}
-CCOnAuthActionFinishedCallback::~CCOnAuthActionFinishedCallback() {}
- 
-CCOnPlayerDataFetchedCallback::CCOnPlayerDataFetchedCallback() {}
-CCOnPlayerDataFetchedCallback::~CCOnPlayerDataFetchedCallback() {}
-
-CCOnAchievementFetchedCallback::CCOnAchievementFetchedCallback() {}
-CCOnAchievementFetchedCallback::~CCOnAchievementFetchedCallback() {}
-
 CloudConnectionCallbackManager::CloudConnectionCallbackManager() {}
 CloudConnectionCallbackManager::~CloudConnectionCallbackManager() {}
 
--- Source/CloudConnectionCallbacks.hpp
+++ Source/CloudConnectionCallbacks.hpp
@@ -3,57 +3,17 @@
 
 #include "VisionBaseIncludes.h"
 
-/// \brief Callback when a sign-in event has begun
-class CLOUDCONNECTIONPLUGIN_IMPEXP CCOnAuthActionStartedCallback : public VCallback
-{
-public:
-  CCOnAuthActionStartedCallback();
-  ~CCOnAuthActionStartedCallback();
-protected:
-private:
-};
-
-/// \brief Callback when a sign-in event ends either successfully or not
-class CLOUDCONNECTIONPLUGIN_IMPEXP CCOnAuthActionFinishedCallback : public VCallback
-{
-public:
-  CCOnAuthActionFinishedCallback();
-  ~CCOnAuthActionFinishedCallback();
-protected:
-private:
-};
-
-/// \brief Callback when a the signed-in player data (such as the display name and Id) have been succesfully fetched from the network
-class CLOUDCONNECTIONPLUGIN_IMPEXP CCOnPlayerDataFetchedCallback : public VCallback
-{
-public:
-  CCOnPlayerDataFetchedCallback();
-  ~CCOnPlayerDataFetchedCallback();
-protected:
-private:
-};
-
-/// \brief Callback when a single achievement has been succesfully fetched from the network
-class CLOUDCONNECTIONPLUGIN_IMPEXP CCOnAchievementFetchedCallback : public VCallback
-{
-public:
-  CCOnAchievementFetchedCallback();
-  ~CCOnAchievementFetchedCallback();
-protected:
-private:
-};
-
 /// \brief Manager for all the callbacks required
-class CLOUDCONNECTIONPLUGIN_IMPEXP CloudConnectionCallbackManager
+class CloudConnectionCallbackManager
 {
 public:
   CloudConnectionCallbackManager();
   ~CloudConnectionCallbackManager();
 
-  CCOnAuthActionStartedCallback OnAuthActionStarted;       ///< called when a sign-in event begins
-  CCOnAuthActionFinishedCallback OnAuthActionFinished;     ///< called when a sign-in event ends
-  CCOnPlayerDataFetchedCallback OnPlayerDataFetched;       ///< called when a player data has been fetched
-  CCOnAchievementFetchedCallback OnAchievementFetched;     ///< called when a single achievement has been succesfully fetched from the network
+  VisCallback_cl OnAuthActionStarted;      ///< called when a sign-in event begins
+  VisCallback_cl OnAuthActionFinished;     ///< called when a sign-in event ends
+  VisCallback_cl OnPlayerDataFetched;      ///< called when a player data has been fetched
+  VisCallback_cl OnAchievementFetched;     ///< called when a single achievement has been succesfully fetched from the network
 protected:
 private:
 };

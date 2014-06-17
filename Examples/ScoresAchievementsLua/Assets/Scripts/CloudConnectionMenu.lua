3539e371179ab6de4c49dadb119098c977979e92
davepoo <davepoo@hotmail.com>
2014-06-17 15:13:16 +0100

    * fix: if the lua script callbacks are added to the same script as the xml GUI script then there are errors on exit "This object is already disposed. It must not be disposed twice"

--- Examples/ScoresAchievementsLua/Assets/Scripts/CloudConnectionMenu.lua
+++ Examples/ScoresAchievementsLua/Assets/Scripts/CloudConnectionMenu.lua
@@ -11,23 +11,13 @@ local ACHIEVEMENT_ID = "CggI9__cunsQAhAB"
 --get this from the google developer console when you create your leaderboard
 local LEADERBOARD_ID = "CggI9__cunsQAhAC" 
 
-function OnAfterSceneLoaded()  
-  -- adding listener for CloudConnection callbacks for to this script
-  local ccClient = CloudConnection:GetClient()
-  ccClient:AddScriptCallbackListener()    
-         
+function OnAfterSceneLoaded()           
   -- Enable debug output in the log file so we can see that callbacks are working
   Debug:Enable(true)
      
   -- test that we can get an acheivement via 'OnAchievementFetched' callback
+    local ccClient = CloudConnection:GetClient()  
   ccClient:GetAchievement( ACHIEVEMENT_ID )
-end
-
-function OnBeforeSceneUnloaded()
-  
-  -- removing listener for CloudConnection callbacks for to this script
-  local ccClient = CloudConnection:GetClient()
-  ccClient:RemoveScriptCallbackListener()    
 end
 
 -- called when an item in the menu is clicked
@@ -37,97 +27,19 @@ function OnItemClicked(self, uiItem, buttons, mousePosX, mousePosY)
         
   -- action the button that was pressed
   if uiItem:GetID() == GUI:GetID("SIGNIN") then  
-    -- show the user sign-in dialog
-    ccClient:BeginUserInitiatedSignIn() 
-    
+    ccClient:BeginUserInitiatedSignIn()               -- show the user sign-in dialog
   elseif uiItem:GetID() == GUI:GetID("SIGNOUT") then  
-    -- signs the user out
-    ccClient:SignOut()
-    
-  elseif uiItem:GetID() == GUI:GetID("ALLACH") then  
-    -- show the UI for all achievements for the signed in player
-    ccClient:ShowAchievements()
-    
-  elseif uiItem:GetID() == GUI:GetID("UNLOCKACH") then  
-    -- unlocks an achievements for the signed in player
-    ccClient:UnlockAchievement( ACHIEVEMENT_ID )
-    
+    ccClient:SignOut()                                -- signs the user out    
+  elseif uiItem:GetID() == GUI:GetID("ALLACH") then      
+    ccClient:ShowAchievements()                       -- show the UI for all achievements for the signed in player
+  elseif uiItem:GetID() == GUI:GetID("UNLOCKACH") then      
+    ccClient:UnlockAchievement( ACHIEVEMENT_ID )      -- unlocks an achievements for the signed in player
   elseif uiItem:GetID() == GUI:GetID("ALLLEAD") then  
-    -- shows the UI for all leaderboards for the signed in player
-    ccClient:ShowLeaderboards()
-    
+    ccClient:ShowLeaderboards()                       -- shows the UI for all leaderboards for the signed in player
   elseif uiItem:GetID() == GUI:GetID("SUBMITSCORE") then  
     -- submits a high score to a leaderboard
     local highScore = 12346;      
-    ccClient:SubmitScore( LEADERBOARD_ID, highScore )
-    
+    ccClient:SubmitScore( LEADERBOARD_ID, highScore )    
   end
   
 end
-
---This callback is made to the script when the Cloud Connection
---Client has started the authorisation process
-function OnAuthActionStarted()
-  Debug:Log("OnAuthActionStarted callback was successfully made to Lua script")
-end
-
---This callback is made to the script when the Cloud Connection
---Client has finsihed the authorisation process, either succesfully or unsucesfully
-function OnAuthActionFinished()
-  Debug:Log("OnAuthActionFinished callback was successfully made to Lua script")
-end
-
---This callback is made to the script when the Cloud Connection
---Client has retrived the player data, this means that calls
---to GetUserDisplayName() will now return a valid value
-function OnPlayerDataFetched()
-  Debug:Log("OnPlayerDataFetched callback was successfully made to Lua script")
-end
-
---This callback is made to the script when the Cloud Connection
---Client has retrived achievement data via the GetAchievement( id ) call
-function OnAchievementFetched( id, name, description, achtype, achstate, totalsteps, currentsteps )
-  Debug:Log("OnAchievementFetched callback was successfully made to Lua script")
-  Debug:Log("ID=")
-  Debug:Log(id)
-  Debug:Log(name)
-  Debug:Log(description)
-  Debug:Log(achtype)
-  Debug:Log(achstate)
-  Debug:Log(totalsteps)
-  Debug:Log(currentsteps)
-end
-
-
-function AuthenticationChanged()
-
-  if G.basicGUI ~= nil then
-    --Get the instance of the cloud connection client
-    local ccClient = CloudConnection:GetClient()
-    local isAutenticated = ccClient:IsAuthenticated()
-
-    SetButtonStatus("SIGNIN", not isAutenticated );          
-    SetButtonStatus("ALLACH", isAutenticated );        
-    SetButtonStatus("UNLOCKACH", isAutenticated );
-    SetButtonStatus("ALLLEAD", isAutenticated );
-    SetButtonStatus("SUBMITSCORE", isAutenticated );
-    SetButtonStatus("SIGNOUT", isAutenticated );
-  end
-  
-end
-
---[[
-function SetButtonStatus( buttonID, enabled )
-  if G.basicGUI ~= nil then
-
-    --find the text label to display the text in
-	  VMenuItemCollection* items = &spMainDlg->Items();
-
-    --enable the signout button if we are authenticated
-	  VDlgControlBasePtr vdlg = items->FindItem( VGUIManager::GetID( buttonID ) );
-	  VASSERT_MSG( vdlg != NULL, "The dialog does not have a label with ID '%s'", buttonID );
-	  VPushButton* pButton = vstatic_cast<VPushButton*>(vdlg);      	--check this is a VPushButton and set enable/disable
-	  pButton->SetEnabled( enabled ); 
-  end
-end
-]]--
--- Examples/ScoresAchievementsLua/Assets/Scripts/DefaultSceneScript.lua
+++ Examples/ScoresAchievementsLua/Assets/Scripts/DefaultSceneScript.lua
@@ -3,26 +3,82 @@
 -- For tutorial on how to use the GUI system in lua then please see http://www.projectanarchy.com/guis-lua
 
 function OnAfterSceneLoaded()
-
-  local ccClient = CloudConnection:GetClient()
-  local displayName = ccClient:GetUserDisplayName()  
-   --Console:SetVisible(true)
-   --Console:PrintLine(displayName)
-   
+        
    -- initalise the GUI and show it 
   GUI:LoadResourceFile("Dialogs/MenuSystem.xml")
   G.basicGUI = GUI:ShowDialog("Dialogs/CloudConnectionMenu.xml")
   G.basicGUI:SetVisible(true) 
-  GUI:SetCursorVisible(true)                         
-       
+  GUI:SetCursorVisible(true)    
+
+  -- adding listener for CloudConnection callbacks for to this script
+  local ccClient = CloudConnection:GetClient()
+  ccClient:AddScriptCallbackListener()            
+  
 end
 
 function OnBeforeSceneUnloaded()
 
+  -- removing listener for CloudConnection callbacks for to this script
+  local ccClient = CloudConnection:GetClient()
+  ccClient:RemoveScriptCallbackListener()   
+
+  -- close the menu
   local mode = Application:GetEditorMode()
   if mode == Vision.EDITOR_PLAY or mode == Vision.EDITOR_RUN then
      GUI:SetCursorVisible(false)
      GUI:CloseDialog( G.basicGUI )
   end
-
 end
+
+--This callback is made to the script when the Cloud Connection
+--Client has started the authorisation process
+function OnAuthActionStarted()
+  Debug:Log("OnAuthActionStarted callback was successfully made to Lua script")
+end
+
+--This callback is made to the script when the Cloud Connection
+--Client has finsihed the authorisation process, either succesfully or unsucesfully
+--this call can also be the result of a sign-out as well as a sign-in attempt
+function OnAuthActionFinished()
+  Debug:Log("OnAuthActionFinished callback was successfully made to Lua script")
+    
+  local ccClient = CloudConnection:GetClient()
+  
+  -- check the authentication
+  if ccClient:IsAuthenticated() then
+    Debug:Log("The user is now authenticated" )
+  else
+    Debug:Log("The user no longer authenticated" )
+  end
+end
+
+--This callback is made to the script when the Cloud Connection
+--Client has retrived the player data, this means that calls
+--to GetUserDisplayName() will now return a valid value
+function OnPlayerDataFetched()
+  Debug:Log("OnPlayerDataFetched callback was successfully made to Lua script")
+      
+  local ccClient = CloudConnection:GetClient()
+  
+  -- check the users display name now we know it is available
+  if ccClient:isAuthenticated() then
+    local displayName = ccClient:GetUserDisplayName()    
+    Debug:Log("The users Display Name is '" .. displayName .. "'" )    
+  end
+end
+
+--This callback is made to the script when the Cloud Connection
+--Client has retrived achievement data via the GetAchievement( id ) call
+function OnAchievementFetched( id, name, description, achtype, achstate, totalsteps, currentsteps )
+  Debug:Log("OnAchievementFetched callback was successfully made to Lua script")
+  Debug:Log("ID=")
+  Debug:Log(id)
+  Debug:Log(name)
+  Debug:Log(description)
+  Debug:Log(achtype)
+  Debug:Log(achstate)
+  Debug:Log(totalsteps)
+  Debug:Log(currentsteps)
+end
+
+

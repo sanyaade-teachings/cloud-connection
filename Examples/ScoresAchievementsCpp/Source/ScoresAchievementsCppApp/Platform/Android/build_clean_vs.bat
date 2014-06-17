b4f996ca3f75b072e0c8e61f224734289d08c623
davepoo <davepoo@hotmail.com>
2014-06-17 19:26:34 +0100

    * fix: visual studio is still sometimes pausing at the end of the build script / making sure the adb start-server is called in the batch files before running any scripts as it is this daemon (being started) that is causing the build process to freeze at the end

--- Examples/ScoresAchievementsCpp/Source/ScoresAchievementsCppApp/Platform/Android/build_clean_vs.bat
+++ Examples/ScoresAchievementsCpp/Source/ScoresAchievementsCppApp/Platform/Android/build_clean_vs.bat
@@ -1,7 +1,10 @@
 REM Cleans the Android buil
 REM This file is designed to be called as a post build event in Visual Studio
 SET ANDROID_HOME=%ADTROOT%\sdk
-cd .\Platform\Android
+
+REM Make sure the adb daemon is started or it can cause the ANT script to pause inside visual studio
+%ANDROID_HOME%\platform-tools\adb.exe start-server
 
 REM Clean the build
+cd .\Platform\Android
 ant clean
--- Examples/ScoresAchievementsLua/Source/ScoresAchievementsLuaApp/platform/Android/build_release_vs.bat
+++ Examples/ScoresAchievementsLua/Source/ScoresAchievementsLuaApp/platform/Android/build_release_vs.bat
@@ -1,7 +1,10 @@
 REM Runs the Android build for java source files
 REM This file is designed to be called as a post build event in Visual Studio
 SET ANDROID_HOME=%ADTROOT%\sdk
-cd .\Platform\Android
+
+REM Make sure the adb daemon is started or it can cause the ANT script to pause inside visual studio
+%ANDROID_HOME%\platform-tools\adb.exe start-server
 
 REM Build in release mode
+cd .\Platform\Android
 ant release -D%1=%2 -D%3=%4 -D%5=%6
